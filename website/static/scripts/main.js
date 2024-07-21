0a23ec5be608ccb7b947e215fc7d1cf7923c82d5
KAL17002493 <74010320+KAL17002493@users.noreply.github.com>
2024-07-21 15:30:50 +0100

    Added buttons for German umlauts under input field whith javaScrtipt to make them functional

--- website/static/scripts/main.js
+++ website/static/scripts/main.js
@@ -1,5 +1,21 @@
 console.log('Hello from main.js');
 
+//Add german characters to the input field when the buttons are clicked
+function addCharacter(event) {
+    // Get the character from the data-char attribute
+    const char = event.target.getAttribute('data-char');
+    // Get the input field
+    const inputField = document.getElementById('guess');
+    // Append the character to the input field's current value
+    inputField.value += char;
+}
+
+// Attach event listeners to all buttons with the class 'addChar'
+const buttons = document.querySelectorAll('.addChar');
+buttons.forEach(button => {
+    button.addEventListener('click', addCharacter);
+});
+
 //Function to focus on the input field when the page loads (mouse stays in the input field after making a word guess)
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("guess").focus();
